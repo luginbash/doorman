75ceec29fdad25904b5591c148a8ec6baf0b0e0d
Luginbash Hiyajo <725033+luginbash@users.noreply.github.com>
2020-02-11 14:17:08 +0800

    Add Ban Callback

--- app/app.py
+++ app/app.py
@@ -3,18 +3,32 @@ import os
 import logging
 import datetime
 import argparse
+import signal
 #import sentry_sdk
 #SENTRY_API = os.environ.get('SENTRY_API')
 #sentry_sdk.init(SENTRY_API)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from typing import NewType
 import telegram
+import json
 
 
 TOKEN = os.environ.get('TOKEN')
 PORT = int(os.environ.get('PORT', '8000'))
 HOST = os.environ.get('HOST', '')
 LISTEN_ADDR = os.environ.get('LISTEN_ADDR', '0.0.0.0')
+
+PERM_NO_NOTHING = telegram.ChatPermissions(
+        can_send_messages=False
+        , can_send_media_messages=False
+        , can_send_polls=False
+        , can_send_other_messages=False
+        , can_add_web_page_previews=False
+        , can_invite_users=False
+        , can_pin_messages=False
+        , can_change_info=False
+    )
+
 
 
 class BotCmd(object):
@@ -66,38 +80,49 @@ def ping(bot, update):
 
 
 @groupMsg
-def welcome(bot:telegram.Bot, ctx):
-    restrictedPerm = telegram.ChatPermissions(
-        can_send_messages=False
-        , can_send_media_messages=False
-        , can_send_polls=False
-        , can_send_other_messages=False
-        , can_add_web_page_previews=False
-        , can_invite_users=False
-        , can_pin_messages=False
-        , can_change_info=False
-    )
-    if ctx.message.new_chat_members:
-        bot.sendMessage(
-            chat_id=ctx.message.chat_id
-            , reply_to_message_id=ctx.message.message_id
-            , text='I hope you\'re not going to spam anyone.'
-        )
-        for usr in ctx.message.new_chat_members:
-            bot.restrict_chat_member(
-                ctx.message.chat_id
+def welcome(update: telegram.Update, ctx: telegram.ext.CallbackContext):
+
+    if update.message.new_chat_members:
+        for usr in update.message.new_chat_members:
+            ctx.bot.restrict_chat_member(
+                update.message.chat_id
                 , user_id=usr.id
-                , until_date=datetime.datetime.now() + datetime.timedelta(minutes=30)
-                , permissions=restrictedPerm
+                , permissions=PERM_NO_NOTHING
             )
+            ctx.bot.sendMessage(
+                chat_id=update.message.chat_id
+                , reply_to_message_id=update.message.message_id
+                , text='Prove that you\'re not a degenerate bot.'
+            )
+            ctx.job_queue.run_once(callbackKick,
+                                   18,
+                                   [update.message.chat_id
+                                       ,usr])
 
 
-def main(token: str, listen_addr: str, port: int, hostname: str) -> None:
+def callbackKick(ctx:telegram.ext.CallbackContext):
+    ctx.bot.kick_chat_member(chat_id=ctx.job.context[1]
+                             , user_id=ctx.job.context[2])
+
+
+def main(token: str, listen_addr: str, port: int, hostname: str, useNgork: bool) -> None:
+
     updater = Updater(token)
+    j = updater.job_queue
 
     botcmd.register_onto(updater.dispatcher)
     groupMsg.register_onto(updater.dispatcher)
-    webhook_url = f"https://{hostname}/{token}"
+
+    if useNgork:
+      from pyngrok import ngrok
+      webhook_url = ngrok.connect(port=port)
+      hostname = webhook_url.replace('http://','',1)
+
+      webhook_url = f"https://{hostname}/{token}"
+
+    with open('../config.json') as f:
+        conf = json.load(f)
+
     print(webhook_url)
     updater.start_webhook(
         listen=listen_addr,
@@ -118,6 +143,8 @@ if __name__ == '__main__':
     parser.add_argument('--host', dest='HOST')
     parser.add_argument('--port', dest='PORT', type=int, default='8000')
     parser.add_argument('--listen', dest='LISTEN_ADDR', default='127.0.0.1')
+    parser.add_argument('--with-ngrok', dest='USE_NGROK', default=True)
     args = parser.parse_args()
 
-    main(token=args.TOKEN, listen_addr=args.LISTEN_ADDR, port=args.PORT, hostname=args.HOST)
+    main(token=args.TOKEN, listen_addr=args.LISTEN_ADDR, port=args.PORT, hostname=args.HOST, useNgork=args.USE_NGROK)
+
